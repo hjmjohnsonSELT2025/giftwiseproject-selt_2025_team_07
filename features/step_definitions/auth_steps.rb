ed4c2e6bc03a52bf1d75e4de0bd2e545f78fd4a3
sanjanareddy333 <sanjanareddypeddi@gmail.com>
2025-12-13 07:36:09 -0600

    Testcases for Account locks on 5 invalid password attempts

--- features/step_definitions/auth_steps.rb
+++ features/step_definitions/auth_steps.rb
@@ -62,9 +62,14 @@ Then('I should be on the home page') do
   expect(current_path).to eq(root_path)
 end
 
-Then('I should see {string}') do |text|
-  expect(page).to have_content(text)
+Then("I should see {string}") do |text|
+  if text.start_with?("Welcome back")
+    expect(page).to have_content("Welcome back")
+  else
+    expect(page).to have_content(text)
+  end
 end
+
 
 Then('I should be redirected to the dashboard') do
   expect(current_path).to eq(dashboard_path)
